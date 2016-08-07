f82a8283bb7635af0c9009fd1eac5ac1e79717cd
Nina Borys <ninaborys5@gmail.com>
2016-08-07 12:29:16 +0300

    class Comment created

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-      Hello! It is a commentList
+      <Comment author="Nikita Pukita">This is first cooment</Comment>
+      <Comment author="Nina Konina">This is *another* one</Comment>
       </div>
     );
   }
@@ -23,14 +24,25 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className="CommentForm">
         My name is CommentForm, hello!
       </div>
     );
   }
 });
 
-
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+        {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
   <CommentBox />,
