1503c1f70d41c51aa9d2667c87ed3e0cb431d75d
Nina Borys <ninaborys5@gmail.com>
2016-08-07 12:16:20 +0300

    CommentList, CommentForm classes created; CommentBox updated

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -2,11 +2,35 @@ var CommentBox = React.createClass({
   render: function() {
       return (
         <div className="commentBox">
-          It is a commentBox named Sraka
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
         </div>
       );
     }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+      Hello! It is a commentList
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+        My name is CommentForm, hello!
+      </div>
+    );
+  }
+});
+
+
 
 ReactDOM.render(
   <CommentBox />,
